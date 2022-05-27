ba400f43d360987f7eb199fcd498031e39418d37
Nguyễn Minh An <minhan112001@gmail.com>
2022-05-27 17:13:19 +0700

    Config for loading data into front-end

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,13 @@
-const app = require('./app');
+const app = require("./app");
 const PORT = process.env.PORT || 8000;
 
-const http = require('http');
-const server = http.createServer(app)
-
-server.listen(PORT, () => { 
+const { loadPlanetData } = require("./models/planets.model.js");
+const http = require("http");
+const server = http.createServer(app);
+async function startServer() {
+  await loadPlanetData();
+  server.listen(PORT, () => {
     console.log(`Server is starting at ${PORT}...`);
-})
-
-
+  });
+}
+startServer();
