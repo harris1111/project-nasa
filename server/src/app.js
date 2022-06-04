060cfa98402ad694097ec2f5b0d826dd9fb714e5
Nguyễn Minh An <minhan112001@gmail.com>
2022-06-04 17:19:28 +0700

    Deploy client to server folder

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors')
+const path = require('path')
 const planetsRouter = require('./routes/planets/planet.router.js');
 const app = express();
 
@@ -7,5 +8,9 @@ app.use(cors( {
     origin: 'http://localhost:3000'
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'..','public')));
+app.get('/', (req,res) => { 
+    res.sendFile(path.join(__dirname,'..','public','index.html'));
+});
 app.use(planetsRouter);
 module.exports = app;
